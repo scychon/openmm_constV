37b667251829d15946391e0de2abbdf0934ae92d
JGaed <60136128+JGaed@users.noreply.github.com>
2021-03-04 12:00:08 +0100

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -29,7 +29,7 @@ setup(name='constvplugin',
       version='1.0',
       py_modules=['constvplugin'],
       ext_modules=[extension],
-      packages=["mdtools"]
-      data_files= []
+      packages=["mdtools"],
+      data_files= [],
       package_data={"mdtools" : []}
      )
